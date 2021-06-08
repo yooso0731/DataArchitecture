4c5688216df0c2cd3312922fcc53ba2c27c8c176
yooso <70804327+yooso0731@users.noreply.github.com>
2021-06-08 16:42:46 +0900

    Update mymodel.py

--- src/mymodel.py
+++ src/mymodel.py
@@ -112,11 +112,8 @@ def run_model1(logger, N=5):
             col_recommend.update_one(
              {"Book": book_id},
              {"$set": {"similar_list": sim_list} })
-<<<<<<< HEAD
-            logger.info('{} -- update similar book {}, score: {}'.format(book_id, sim_book_id, score))
-=======
-             #logger.info('{} -- update similar book {}, score: {}'.format(book_id, sim_book_id, score))
->>>>>>> 6b8d1dc1cb2b5bb7859552c140d15d5830fd674f
+            #logger.info('{} -- update similar book {}, score: {}'.format(book_id, sim_book_id, score))
+
         
     db_client.close()
 
